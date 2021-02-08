b50cdc9d2f9d046a541a8c3b64307c9f33ad297b
Anna1Lin <annalincc@gmail.com>
2021-02-09 00:43:28 +0800

    write products names and price to csv file

--- products.py
+++ products.py
@@ -10,3 +10,10 @@ print(products)
 
 print(products[0][0])
 print(products[1][0])
+
+for p in products:
+    print(p[0], "cost", p[1])
+
+with open ("products.csv", "w") as f:
+    for p in products:
+        f.write(p[0] + "," + p[1] + "\n")
