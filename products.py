c3b6885b8eeb7dd16b670bf03a0dbbafbedab1b7
Anna1Lin <annalincc@gmail.com>
2021-02-11 17:22:44 +0800

    add encoding and change the type of string+int

--- products.py
+++ products.py
@@ -4,8 +4,8 @@ while True:
     if name == "q":
         break
     price = input("please input product price: ")
-    p = [name, price]
-    products.append(p)
+    price = int(price)
+    products.append([name, price])
 print(products)
 
 print(products[0][0])
@@ -14,6 +14,7 @@ print(products[1][0])
 for p in products:
     print(p[0], "cost", p[1])
 
-with open ("products.csv", "w") as f:
+with open ("products.csv", "w", encoding="utf-8") as f:
+    f.write("The product name, The price \n")
     for p in products:
-        f.write(p[0] + "," + p[1] + "\n")
+        f.write(p[0] + "," + str(p[1]) + "\n")
